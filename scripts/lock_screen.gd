84654257160249e0e658c49159df9cbe12ff396d
fivesixfive <contact@fivesixfive.dev>
2023-03-31 16:55:04 -0500

    updated game

--- scripts/lock_screen.gd
+++ scripts/lock_screen.gd
@@ -1,10 +1,18 @@
 extends Control
 func _on_Submit_pressed():
-	if get_node("%Code").visible:
+	# Quit
+	if get_node("%Username").text == "Secret shutdown":
+		get_tree().quit()
+	# Reset
+	elif get_node("%Code").visible:
 		get_node("%Code").visible = false
 		get_node("%Error").visible = false
+		get_node("%Username").text = ""
+		get_node("%Password").text = ""
+	# Correct
 	elif get_node("%Username").text.to_lower() != "tiberius" or get_node("%Password").text.to_lower() != "#secret*society":
 		get_node("%Error").visible = true
+	# Error 
 	else:
 		get_node("%Code").visible = true
 		get_node("%Error").visible = false
