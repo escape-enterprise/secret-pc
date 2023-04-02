72c0d20e1cd5567c2e939db8d231ada993dd7414
fivesixfive <contact@fivesixfive.dev>
2023-04-02 16:59:30 -0500

    bug fixes / visual update

--- scripts/lock_screen.gd
+++ scripts/lock_screen.gd
@@ -1,18 +1,34 @@
 extends Control
 func _on_Submit_pressed():
-	# Quit
-	if get_node("%Username").text == "Secret shutdown":
-		get_tree().quit()
-	# Reset
-	elif get_node("%Code").visible:
-		get_node("%Code").visible = false
-		get_node("%Error").visible = false
-		get_node("%Username").text = ""
-		get_node("%Password").text = ""
-	# Correct
-	elif get_node("%Username").text.to_lower() != "tiberius" or get_node("%Password").text.to_lower() != "#secret*society":
-		get_node("%Error").visible = true
-	# Error 
-	else:
-		get_node("%Code").visible = true
-		get_node("%Error").visible = false
+	# Reset error
+	get_node("%Error").visible = false
+	
+	# Check for reset
+	if get_node("%Code").visible:
+		clear(false)
+		return
+	
+	# Check username matching
+	match get_node("%Username").text.to_lower():
+		# Correct
+		"tiberius":
+			if get_node("%Password").text.to_lower() == "#secret*society":
+				get_node("%Code").visible = true
+				return
+		
+		# Shutdown
+		"secret shutdown":
+			get_tree().quit()
+			return
+			
+		"":
+			clear(false)
+			return
+
+	clear(true)
+
+func clear(error):
+	get_node("%Error").visible = error
+	get_node("%Code").visible = false
+	get_node("%Username").text = ""
+	get_node("%Password").text = ""
